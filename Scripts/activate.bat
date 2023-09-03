863aa61a0236643e626d1d6c88d1ca893a9e054c
Michael <499674+neoxyte@users.noreply.github.com>
2023-09-03 06:26:38 -0400

    IDEK

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Michael Work\OneDrive\Desktop\KeepaScrape
+set VIRTUAL_ENV=C:\Users\mikeh\OneDrive\Desktop\KeepaScrape
 
 if not defined PROMPT set PROMPT=$P$G
 
